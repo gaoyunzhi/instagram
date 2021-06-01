9a34d7af6d68920d14ca15650d91d181046acfc3
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-05-31 19:24:01 -0700

    commit

--- to_album/__init__.py
+++ to_album/__init__.py
@@ -6,12 +6,15 @@ name = 'instagram_to_album'
 from telegram_util import AlbumResult as Result
 
 def getImgs(content):
-	for node in content['edges']:
-		yield node['node']['images']['standard_resolution']['url']
+    for node in content['edges']:
+        yield node['node']['images']['standard_resolution']['url']
 
 def get(content):
     result = Result()
     result.url = content['link']
     result.cap_html_v2 = content['caption']['text']
-    result.imgs = list(getImgs(content['edge_sidecar_to_children']))
+    if 'edge_sidecar_to_children' in content:
+        result.imgs = list(getImgs(content['edge_sidecar_to_children']))
+    else:
+        result.imgs = [content['images']['standard_resolution']['url']]
     return result
