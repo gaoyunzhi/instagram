7a643c7228bae2bb23dd2d34849eeee0dae29644
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-07-09 17:57:24 -0700

    commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+def test():
+	count = 0
+	target = list(range(0, 20))
+	with open('candidates.txt') as f:
+		for line in f:
+			count += 1
+			if count not in target:
+				continue
+			line = line.strip()
+			if not line:
+				continue
+			os.system('open ' + line.strip() + ' -g')
+
+if __name__ == '__main__':
+	test()
